8ab9cd4dd00f7b899eb486f6e1569ef34db12ae0
He-Red-Bo <1196450116@qq.com>
2016-03-17 02:37:28 +0800

    提交落下的代码

---
+++ blog/protected/views/layouts/blog.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Yii2文章管理系统</title>
+
+<link href="<?php echo CSS_URL;  ?>common.css" rel="stylesheet" />
+</head>	
+<body>
+	<div id="top">
+	</div>
+	<div id="header">
+		<div class='logo'>
+			<a href=""><img src="<?php echo IMG_URL; ?>logo.jpg" alt=""></a>
+		</div>
+		<div class='navigation'>
+			<a href="">首页</a>
+			
+		</div>
+	</div>
+
+	<?php echo $content ?>
+	
+	<div class='sidebar'>
+			<div class='item'>
+				<h2>文章标题</h2>
+				<ul class='flink'>
+					
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+	<div id="footer">
+		<div class='bgbar'></div>
+		<div class='bottom'>
+			<div class='pos'>
+				<div class='copyright'>
+					© Copyright 2011-2013 www.houdunwang 后盾网
+				</div>
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
